1e93fe122d0e7edeaa35e9224318cbf32329df25
Netra Sriram <netrasriram30@gmail.com>
2026-09-19 12:27:13 +0530

    Week 6 Assignment Q3: Premium Loyalty Discount

--- src/week6/assignment/Q3.java
+++ src/week6/assignment/Q3.java
@@ -1,4 +1,11 @@
 package week6.assignment;
-
-public class Q3 {
+public class Q3{
+    public static void main(String[] args){
+        PremiumMember p=new PremiumMember("MEM5",2000,"Coach Riya");
+        p.chargeLateFee(200);
+        System.out.println(p.getTotalLateFees());
+        int[] history=p.getLateFeeHistory();
+        history[0]=999;
+        System.out.println(java.util.Arrays.toString(p.getLateFeeHistory()));
+    }
 }

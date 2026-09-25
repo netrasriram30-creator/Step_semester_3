3b5140d9f46f3c8848fd8634fa2beaa9cd1e34ac
Netra Sriram <netrasriram30@gmail.com>
2026-09-26 01:10:31 +0530

    Week 5 Assignment Problems

---
+++ src/week5/assignment/Q2.java
@@ -0,0 +1,47 @@
+package week5.assignment;
+public class Q2{
+    public static String classifyAccess(String fieldModifier,String accessorContext){
+        if(fieldModifier.equals("private")){
+            if(accessorContext.equals("SAME_CLASS")){
+                return "ALLOWED";
+            }
+        }
+        else if(fieldModifier.equals("default")){
+            if(accessorContext.equals("SAME_CLASS")||accessorContext.equals("SAME_PACKAGE")){
+                return "ALLOWED";
+            }
+        }
+        else if(fieldModifier.equals("protected")){
+            if(accessorContext.equals("SAME_CLASS")||accessorContext.equals("SAME_PACKAGE")||accessorContext.equals("SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE")){
+                return "ALLOWED";
+            }
+        }
+        else if(fieldModifier.equals("public")){
+            return "ALLOWED";
+        }
+        return "DENIED";
+    }
+    public static String firstDeniedAttempt(String[][] attempts){
+        for(int i=0;i<attempts.length;i++){
+            String modifier=attempts[i][0];
+            String context=attempts[i][1];
+            if(classifyAccess(modifier,context).equals("DENIED")){
+                return modifier+" via "+context+" (attempt #"+(i+1)+")";
+            }
+        }
+        return "None Denied";
+    }
+    public static void main(String[] args){
+        String[][] attempts1={
+                {"public","SUBCLASS_DIFFERENT_PACKAGE_PARENT_TYPE"},
+                {"protected","SUBCLASS_DIFFERENT_PACKAGE_PARENT_TYPE"},
+                {"protected","SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE"}
+        };
+        System.out.println(firstDeniedAttempt(attempts1));
+        String[][] attempts2={
+                {"public","SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE"},
+                {"protected","SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE"}
+        };
+        System.out.println(firstDeniedAttempt(attempts2));
+    }
+}
---
+++ src/week5/assignment/Q4.java
@@ -0,0 +1,53 @@
+package week5.assignment;
+public class Q4{
+    private String membershipId;
+    private String name;
+    private boolean premiumMember;
+    private String securityAnswer;
+    private boolean membershipIdSet;
+    public Q4(){
+        membershipIdSet=false;
+    }
+    public String getMembershipId(){
+        return membershipId;
+    }
+    public void setMembershipId(String id){
+        if(!membershipIdSet){
+            membershipId=id;
+            membershipIdSet=true;
+        }
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public boolean isPremiumMember(){
+        return premiumMember;
+    }
+    public void setPremiumMember(boolean premium){
+        premiumMember=premium;
+    }
+    public void setSecurityAnswer(String answer){
+        securityAnswer=transform(answer);
+    }
+    private String transform(String answer){
+        StringBuilder result=new StringBuilder();
+        for(int i=0;i<answer.length();i++){
+            result.append((char)(answer.charAt(i)+1));
+        }
+        return result.toString();
+    }
+    public static void main(String[] args){
+        Q4 m=new Q4();
+        m.setMembershipId("LIB-8841");
+        m.setName("Priya Nair");
+        m.setPremiumMember(true);
+        System.out.println(m.getMembershipId());
+        m.setMembershipId("FAKE-0000");
+        System.out.println(m.getMembershipId());
+        System.out.println(m.isPremiumMember());
+        m.setSecurityAnswer("BlueMountain");
+    }
+}
